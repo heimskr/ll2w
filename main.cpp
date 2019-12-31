200b8e8f253eb92d6d580f3549a0342db47fb18e
Kai Tamkun <ktamkun@ucsc.edu>
2019-12-31 18:06:23 +0100

    Add a delay before changing the graph.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include <unistd.h>
+
 #include "Graph.h"
 
 int main(int argc, char **argv) {
@@ -17,6 +19,7 @@ int main(int argc, char **argv) {
 	graph.addEdges("one:two two:three three:four four:one one:four one:three two:four three:three");
 	std::cout << graph.toDot();
 	graph.renderTo("graph1234.png");
+	usleep(100000);
 	graph -= "four";
 	std::cout << graph.toDot();
 	graph.renderTo("graph123.png");
