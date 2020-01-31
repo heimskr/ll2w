d717ba2d986351d6b3327226ec07abed0e1a85a5
Kai Tamkun <ktamkun@ucsc.edu>
2020-01-30 18:21:40 -0800

    Simplify Instruction::extract

--- compiler/Instruction.cpp
+++ compiler/Instruction.cpp
@@ -3,49 +3,70 @@
 #include "compiler/Instruction.h"
 #include "util/Util.h"
 
+#define IFLV(x) if (const LocalValue *local_value = dynamic_cast<const LocalValue *>((x))) readname(local_value)
+#define FORV(x...) for (const Value *value: {x})
+
 namespace LL2W {
 	std::pair<char, char> Instruction::extract() {
+		read.clear();
+		written.clear();
+
+		auto readname = [&](const LocalValue *lv) {
+			read.push_back(parseLong(lv->name));
+		};
+
+		auto write = [&](const std::string *str) {
+			written.push_back(parseLong(str));
+		};
+
 		switch (node->nodeType()) {
 			case NodeType::Select: {
 				const SelectNode *select = dynamic_cast<const SelectNode *>(node);
-				int readCount = 0;
-				for (const Value *value: {select->firstValue, select->secondValue}) {
-					if (const LocalValue *local_value = dynamic_cast<const LocalValue *>(value)) {
-						readCount++;
-						read.push_back(parseLong(local_value->name));
-					}
+				FORV(select->firstValue, select->secondValue) {
+					IFLV(value);
 				}
 				written.push_back(parseLong(select->result));
-				return {readCount, 1};
+				break;
 			}
 
 			case NodeType::Alloca: {
 				const AllocaNode *alloca = dynamic_cast<const AllocaNode *>(node);
-				written.push_back(parseLong(alloca->result));
-				if (const LocalValue *local_value = dynamic_cast<const LocalValue *>(alloca->numelementsValue)) {
-					read.push_back(parseLong(local_value->name));
-					return {1, 1};
-				}
-				return {0, 1};
+				write(alloca->result);
+				IFLV(alloca->numelementsValue);
+				break;
 			}
 
 			case NodeType::Store: {
 				const StoreNode *store = dynamic_cast<const StoreNode *>(node);
-				int readCount = 0;
-				for (const Value *value: {store->value, store->constant->value}) {
-					if (const LocalValue *local_value = dynamic_cast<const LocalValue *>(value)) {
-						readCount++;
-						read.push_back(parseLong(local_value->name));
-					}
-				}
-				return {readCount, 0};
+				FORV(store->value, store->constant->value)
+					IFLV(value);
+				break;
 			}
 
-			default:;
+			case NodeType::Load: {
+				const LoadNode *load = dynamic_cast<const LoadNode *>(node);
+				write(load->result);
+				IFLV(load->constant->value);
+				break;
+			}
+
+			case NodeType::Icmp: {
+				const IcmpNode *icmp = dynamic_cast<const IcmpNode *>(node);
+				write(icmp->result);
+				FORV(icmp->value1, icmp->value2)
+					IFLV(value);
+				break;
+			}
+
+			default:
+				return {-1, -1};
 				// std::cout << "\e[2m[\e[0;33m!\e[0;2m]\e[0m Unknown instruction type: "
 				//           << static_cast<int>(node->nodeType());
 		}
 
-		return {-1, -1};
+		return {read.size(), written.size()};
 	}
 }
+
+#undef IFLV
+#undef FORV
