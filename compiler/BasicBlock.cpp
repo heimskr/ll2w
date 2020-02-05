5a78caec607abfe550d89a37fff4ae7cd5a176d1
Kai Tamkun <ktamkun@ucsc.edu>
2020-02-05 01:17:43 -0800

    Add BasicBlock::extract

--- compiler/BasicBlock.cpp
+++ compiler/BasicBlock.cpp
@@ -5,4 +5,22 @@ namespace LL2W {
 	BasicBlock::BasicBlock(int label_, const std::vector<int> &preds_,
 	                       const std::vector<std::shared_ptr<Instruction>> &instructions_):
 		label(label_), preds(preds_), instructions(instructions_) {}
+
+	std::pair<char, char> BasicBlock::extract(bool force) {
+		if (extracted && !force)
+			return {read.size(), written.size()};
+
+		read.clear();
+		written.clear();
+
+		for (std::shared_ptr<Instruction> &instruction: instructions) {
+			instruction->extract();
+			for (int read_var: instruction->read)
+				read.insert(read_var);
+			for (int written_var: instruction->written)
+				written.insert(written_var);
+		}
+
+		return {read.size(), written.size()};
+	}
 }
--- include/compiler/BasicBlock.h
+++ include/compiler/BasicBlock.h
@@ -2,6 +2,7 @@
 #define COMPILER_BASICBLOCK_H_
 
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "parser/Nodes.h"
@@ -10,12 +11,18 @@ namespace LL2W {
 	class Instruction;
 
 	class BasicBlock {
+		private:
+			bool extracted = false;
+
 		public:
 			int label;
 			std::vector<int> preds;
 			std::vector<std::shared_ptr<Instruction>> instructions;
+			std::unordered_set<int> read, written;
 
 			BasicBlock(int, const std::vector<int> &, const std::vector<std::shared_ptr<Instruction>> &);
+
+			std::pair<char, char> extract(bool force = false);
 	};
 }
 
