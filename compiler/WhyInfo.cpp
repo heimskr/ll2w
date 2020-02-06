56753f32056028456bbabc457c7d85a31e18a9f5
Kai Tamkun <ktamkun@ucsc.edu>
2020-02-06 12:02:39 -0800

    Add compiler/WhyInfo

---
+++ compiler/WhyInfo.cpp
@@ -0,0 +1,7 @@
+#include "compiler/WhyInfo.h"
+
+namespace LL2W {
+	int WhyInfo::savedCount = 23;
+	int WhyInfo::temporaryCount = 23;
+	int WhyInfo::assemblerCount = 16;
+}
---
+++ include/compiler/WhyInfo.h
@@ -0,0 +1,11 @@
+#ifndef COMPILER_WHYINFO_H_
+#define COMPILER_WHYINFO_H_
+
+namespace LL2W {
+	/** Contains constants pertaining to the Why.js architecture. */
+	struct WhyInfo {
+		static int savedCount, temporaryCount, assemblerCount;
+	};
+}
+
+#endif
