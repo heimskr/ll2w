3d23b4b53861c57c1d9352b8f5f7cd862cbfc095
Kai Tamkun <ktamkun@ucsc.edu>
2020-02-17 20:35:34 -0800

    Add register offsets to WhyInfo

--- compiler/WhyInfo.cpp
+++ compiler/WhyInfo.cpp
@@ -3,9 +3,21 @@
 namespace LL2W {
 	int WhyInfo::wordSize = 8;
 
+	int WhyInfo::savedOffset = 62;
 	int WhyInfo::savedCount = 23;
+
+	int WhyInfo::temporaryOffset = 39;
 	int WhyInfo::temporaryCount = 23;
+
+	int WhyInfo::assemblerOffset = 102;
 	int WhyInfo::assemblerCount = 16;
+
+	int WhyInfo::argumentOffset = 23;
+	int WhyInfo::argumentCount = 16;
+
+	int WhyInfo::stackPointerOffset = 2;
+	int WhyInfo::framePointerOffset = 3;
+	int WhyInfo::returnAddressOffset = 4;
 
 	int WhyInfo::halfFloatWidth = 4;
 	int WhyInfo::floatWidth = 8;
--- include/compiler/WhyInfo.h
+++ include/compiler/WhyInfo.h
@@ -5,7 +5,9 @@ namespace LL2W {
 	/** Contains constants pertaining to the Why.js architecture. */
 	struct WhyInfo {
 		static int wordSize; // in bytes
-		static int savedCount, temporaryCount, assemblerCount;
+		static int savedOffset, temporaryOffset, argumentOffset, assemblerOffset;
+		static int savedCount,  temporaryCount,  argumentCount,  assemblerCount;
+		static int stackPointerOffset, framePointerOffset, returnAddressOffset;
 		static int halfFloatWidth, floatWidth, doubleWidth, pointerWidth; // in bytes
 	};
 }
