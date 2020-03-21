cbe20658b6fe680191aad8c5e8f8b374045e497a
Kai Tamkun <ktamkun@ucsc.edu>
2020-03-20 21:31:57 -0700

    Whoops that's a template class

--- include/instruction/JType.h
+++ include/instruction/JType.h
@@ -13,8 +13,28 @@ namespace LL2W {
 		JType(std::shared_ptr<Variable> rs_, T addr_, bool link_, int index_ = -1):
 			WhyInstruction(index_), rs(rs_), addr(addr_), link(link_) {}
 
-		ExtractionResult extract(bool force = false) override;
-		bool replaceRead(std::shared_ptr<Variable>, std::shared_ptr<Variable>) override;
+		ExtractionResult extract(bool force = false) override {
+		if (extracted && !force)
+			return {read.size(), 0};
+
+		read.clear();
+		written.clear();
+
+		if (rs)
+			read.insert(rs);
+
+		extracted = true;
+		return {read.size(), 0};
+	}
+
+		bool replaceRead(std::shared_ptr<Variable> to_replace, std::shared_ptr<Variable> new_var) override {
+			if (rs == to_replace) {
+				rs = new_var;
+				return true;
+			}
+
+			return false;
+		}
 	};
 }
 
