bbb5bc0ba11257d54a2e26ed3ab5d440772f862c
Kai Tamkun <ktamkun@ucsc.edu>
2020-03-30 15:07:03 -0700

    Add size to StoreSymbolInstruction

--- include/instruction/StoreSymbolInstruction.h
+++ include/instruction/StoreSymbolInstruction.h
@@ -4,9 +4,16 @@
 #include "instruction/IType.h"
 
 namespace LL2W {
+	/**
+	 * $rs -> [&symbol]
+	 */
 	struct StoreSymbolInstruction: public IType<std::string> {
-		StoreSymbolInstruction(std::shared_ptr<Variable> rs_, std::string imm_, int index_ = -1):
-			IType(rs_, imm_, nullptr, index_) {}
+		/** The size of the store in bytes. */
+		int size;
+
+		StoreSymbolInstruction(std::shared_ptr<Variable> rs_, const std::string &imm_, int size_, int index_ = -1):
+			IType(rs_, imm_, nullptr, index_), size(size_) {}
+
 		std::string debugExtra() override;
 	};
 }
--- src/instruction/StoreSymbolInstruction.cpp
+++ src/instruction/StoreSymbolInstruction.cpp
@@ -3,6 +3,13 @@
 
 namespace LL2W {
 	std::string StoreSymbolInstruction::debugExtra() {
-		return std::string(*rs) + " \e[2m-> [\e[22;91m&" + imm + " \e[39;2m]\e[22m";
+		const std::string base = std::string(*rs) + " \e[2m-> [\e[22;91m&" + imm + " \e[39;2m]\e[22m";
+		switch (size) {
+			case 8:  return base;
+			case 4:  return base + " /h";
+			case 2:  return base + " /s";
+			case 1:  return base + " /b";
+			default: return base + " /" + std::to_string(size);
+		}
 	}
 }
