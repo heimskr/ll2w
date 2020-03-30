94e899f3f0cd2a3b010d9c68bf7800cbdcac1515
Kai Tamkun <ktamkun@ucsc.edu>
2020-03-30 14:27:03 -0700

    Whoops, that's the wrong register for si

--- include/instruction/StoreIInstruction.h
+++ include/instruction/StoreIInstruction.h
@@ -4,11 +4,14 @@
 #include "instruction/IType.h"
 
 namespace LL2W {
+	/**
+	 * $rs -> [imm]
+	 */
 	struct StoreIInstruction: public IType<int> {
 		/** The size of the store in bytes. */
 		int size;
 
-		StoreIInstruction(std::shared_ptr<Variable> rd_, int imm_, int size_, int index_ = -1);
+		StoreIInstruction(std::shared_ptr<Variable> rs_, int imm_, int size_, int index_ = -1);
 
 		std::string debugExtra() override;
 	};
--- src/instruction/StoreIInstruction.cpp
+++ src/instruction/StoreIInstruction.cpp
@@ -2,11 +2,11 @@
 #include "instruction/StoreIInstruction.h"
 
 namespace LL2W {
-	StoreIInstruction::StoreIInstruction(std::shared_ptr<Variable> rd_, int imm_, int size_, int index_):
-		IType(nullptr, imm_, rd_, index_), size(size_) {}
+	StoreIInstruction::StoreIInstruction(std::shared_ptr<Variable> rs_, int imm_, int size_, int index_):
+		IType(rs_, imm_, nullptr, index_), size(size_) {}
 
 	std::string StoreIInstruction::debugExtra() {
-		const std::string base = std::string(*rd) + " \e[2m-> [\e[22;91m" + std::to_string(imm) + "\e[39;2m]\e[22m";
+		const std::string base = std::string(*rs) + " \e[2m-> [\e[22;91m" + std::to_string(imm) + "\e[39;2m]\e[22m";
 		switch (size) {
 			case 8:  return base;
 			case 4:  return base + " /h";
