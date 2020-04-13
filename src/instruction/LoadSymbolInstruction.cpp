83780fa54f0b7ac00fcf0bb534751c86c254fb8d
Kai Tamkun <ktamkun@ucsc.edu>
2020-04-13 13:13:36 -0700

    Fix erroneous & in LoadSymbolInstruction

--- src/instruction/LoadSymbolInstruction.cpp
+++ src/instruction/LoadSymbolInstruction.cpp
@@ -7,7 +7,7 @@ namespace LL2W {
 		IType(nullptr, imm_, rd_, index_), size(size_) {}
 
 	std::string LoadSymbolInstruction::debugExtra() {
-		const std::string base = "\e[2m[\e[0;91m&" + imm + "\e[0;2m] ->\e[0m " + std::string(*rd);
+		const std::string base = "\e[2m[\e[0;91m" + imm + "\e[0;2m] ->\e[0m " + std::string(*rd);
 		switch (size) {
 			case 8:  return base;
 			case 4:  return base + " /h";
@@ -18,7 +18,7 @@ namespace LL2W {
 	}
 
 	std::string LoadSymbolInstruction::toString() const {
-		const std::string base = "[&" + imm + "] -> " + rd->toString();
+		const std::string base = "[" + imm + "] -> " + rd->toString();
 		switch (size) {
 			case 8:  return base;
 			case 4:  return base + " /h";
