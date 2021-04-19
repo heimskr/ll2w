a7eec14ed7967984386e8e0ec813edc6b504ece0
Kai Tamkun <ktamkun@ucsc.edu>
2021-04-19 01:55:41 -0700

    Add some pretty colors

--- src/wasm/Nodes.cpp
+++ src/wasm/Nodes.cpp
@@ -3,13 +3,25 @@
 #include "parser/StringSet.h"
 #include "wasm/Nodes.h"
 
+static std::string cyan(const std::string &interior) {
+	return "\e[36m" + interior + "\e[39m";
+}
+
+static std::string dim(const std::string &interior) {
+	return "\e[2m" + interior + "\e[22m";
+}
+
+static std::string green(const std::string &interior) {
+	return "\e[32m" + interior + "\e[39m";
+}
+
 namespace LL2W {
 	RNode::RNode(ASTNode *rs_, ASTNode *oper_, ASTNode *rt_, ASTNode *rd_, ASTNode *unsigned_):
 		WASMBaseNode(wasmParser, WASM_RNODE),
 		rs(rs_->lexerInfo), oper(oper_->lexerInfo), rt(rt_->lexerInfo), rd(rd_->lexerInfo), isUnsigned(!!unsigned_) {}
 
 	std::string RNode::debugExtra() const {
-		return *rs + " " + *oper + " " + *rt + " -> " + *rd + (isUnsigned? " /u" : "");
+		return cyan(*rs) + " " + dim(*oper) + " " + cyan(*rt) + dim(" -> ") + cyan(*rd) + (isUnsigned? " /u" : "");
 	}
 
 	INode::INode(ASTNode *rs_, ASTNode *oper_, ASTNode *imm_, ASTNode *rd_, ASTNode *unsigned_):
@@ -17,27 +29,28 @@ namespace LL2W {
 		rs(rs_->lexerInfo), oper(oper_->lexerInfo), rd(rd_->lexerInfo), imm(imm_->atoi()), isUnsigned(!!unsigned_) {}
 
 	std::string INode::debugExtra() const {
-		return *rs + " " + *oper + " " + std::to_string(imm) + " -> " + *rd + (isUnsigned? " /u" : "");
+		return cyan(*rs) + " " + dim(*oper) + " " + green(std::to_string(imm)) + dim(" -> ") + cyan(*rd)
+		     + (isUnsigned? " /u" : "");
 	}
 
 	WASMCopyNode::WASMCopyNode(ASTNode *rs_, ASTNode *rd_, ASTNode *byte_):
 		WASMBaseNode(wasmParser, WASM_COPYNODE), rs(rs_->lexerInfo), rd(rd_->lexerInfo), isByte(!!byte_) {}
 
 	std::string WASMCopyNode::debugExtra() const {
-		return "[" + *rs + "] -> [" + *rd + "]" + (isByte? " /b" : "");
+		return dim("[") + cyan(*rs) + dim("] -> [") + cyan(*rd) + dim("]") + (isByte? " /b" : "");
 	}
 
 	WASMLoadNode::WASMLoadNode(ASTNode *rs_, ASTNode *rd_, ASTNode *byte_):
 		WASMBaseNode(wasmParser, WASM_LOADNODE), rs(rs_->lexerInfo), rd(rd_->lexerInfo), isByte(!!byte_) {}
 
 	std::string WASMLoadNode::debugExtra() const {
-		return "[" + *rs + "] -> " + *rd + (isByte? " /b" : "");
+		return dim("[") + cyan(*rs) + dim("] -> ") + cyan(*rd) + (isByte? " /b" : "");
 	}
 
 	WASMStoreNode::WASMStoreNode(ASTNode *rs_, ASTNode *rd_, ASTNode *byte_):
 		WASMBaseNode(wasmParser, WASM_STORENODE), rs(rs_->lexerInfo), rd(rd_->lexerInfo), isByte(!!byte_) {}
 
 	std::string WASMStoreNode::debugExtra() const {
-		return *rs + " -> [" + *rd + "]" + (isByte? " /b" : "");
+		return cyan(*rs) + dim(" -> [") + cyan(*rd) + dim("]") + (isByte? " /b" : "");
 	}
 }
