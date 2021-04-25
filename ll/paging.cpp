c11df86c153d422c4cdff6f331a3018077c950ad
Kai Tamkun <ktamkun@ucsc.edu>
2021-04-25 15:54:21 -0700

    Move paging.c to paging.cpp

--- ll/paging.cpp
+++ ll/paging.cpp
@@ -25,14 +25,13 @@ void pagefault() {
 }
 
 void __attribute__((naked)) strprint(const char *str) {
-	asm("                        \
-		@_strprint_loop          \
-		[$a0] -> $ma /b          \
-		: _strprint_print if $ma \
-		: _strprint_done         \
-		@_strprint_print         \
-		<prc $ma>                \
-		$a0 + 1 -> $a0           \
-		: _strprint_loop         \
-		@_strprint_done");
+	asm("@_strprint_loop          \
+	     [$a0] -> $ma /b          \
+	     : _strprint_print if $ma \
+	     : _strprint_done         \
+	     @_strprint_print         \
+	     <prc $ma>                \
+	     $a0 + 1 -> $a0           \
+	     : _strprint_loop         \
+	     @_strprint_done");
 }
