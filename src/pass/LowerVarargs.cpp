96bf2ce44d96e6bcd475c95ca12007db9b86b739
Kai Tamkun <ktamkun@ucsc.edu>
2021-04-27 01:09:02 -0700

    Fix incorrect instruction removal in LowerVarargs

--- src/pass/LowerVarargs.cpp
+++ src/pass/LowerVarargs.cpp
@@ -12,12 +12,10 @@
 
 namespace LL2W::Passes {
 	void lowerVarargsFirst(Function &function) {
-		const std::list<InstructionPtr> &linearInstructions = function.linearInstructions;
 		auto m2 = function.makeAssemblerVariable(2, function.getEntry());
 		bool any_changed = false;
-		for (auto iter = linearInstructions.begin(), end = linearInstructions.end(); iter != end;) {
-		// for (InstructionPtr &instruction: function.linearInstructions) {
-			InstructionPtr instruction = *iter;
+		std::list<InstructionPtr> to_remove;
+		for (InstructionPtr &instruction: function.linearInstructions) {
 			if (LLVMInstruction *llvm = dynamic_cast<LLVMInstruction *>(instruction.get())) {
 				if (CallNode *call = dynamic_cast<CallNode *>(llvm->node)) {
 					if (!call->name->isGlobal())
@@ -35,18 +33,17 @@ namespace LL2W::Passes {
 						function.insertBefore(instruction,
 							std::make_shared<StoreRInstruction>(m2, var, var->type->width() / 8));
 						any_changed = true;
-						iter = ++iter;
-						function.remove(instruction);
+						to_remove.push_back(instruction);
 						continue;
 					} else if (*global->name == "llvm.va_end") {
-						iter = ++iter;
-						function.remove(instruction);
+						to_remove.push_back(instruction);
 					}
 				}
 			}
-
-			++iter;
 		}
+
+		for (auto &instruction: to_remove)
+			function.remove(instruction);
 
 		if (!any_changed)
 			function.variableStore.erase(m2->id);
