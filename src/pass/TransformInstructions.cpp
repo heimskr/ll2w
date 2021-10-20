d4c5e986b3efc66e3d4e1c2e0ee5061122e92ea9
Kai Tamkun <ktamkun@ucsc.edu>
2021-10-20 16:19:00 -0700

    Deal with silly 24-bit loads

--- src/pass/TransformInstructions.cpp
+++ src/pass/TransformInstructions.cpp
@@ -1,5 +1,6 @@
 #include "compiler/Function.h"
 #include "compiler/Instruction.h"
+#include "instruction/AndIInstruction.h"
 #include "instruction/LoadIInstruction.h"
 #include "instruction/LoadRInstruction.h"
 #include "instruction/SetInstruction.h"
@@ -13,16 +14,34 @@ namespace LL2W::Passes {
 
 		for (InstructionPtr &instruction: function.linearInstructions) {
 			if (auto li = std::dynamic_pointer_cast<LoadIInstruction>(instruction)) {
-				if (li->size != 4)
-					continue;
-				// TODO: verify
-				auto var = function.newVariable(std::make_shared<PointerType>(std::make_shared<IntType>(64)),
-					li->parent.lock());
-				auto set = std::make_shared<SetInstruction>(var, li->imm);
-				auto l = std::make_shared<LoadRInstruction>(var, li->rd, li->size);
-				function.insertBefore(instruction, set)->setDebug(li->debugIndex)->extract();
-				function.insertBefore(instruction, l)->setDebug(li->debugIndex)->extract();
-				to_remove.push_back(li);
+				if (li->size == 4) {
+					// TODO: verify
+					auto var = function.newVariable(std::make_shared<PointerType>(std::make_shared<IntType>(64)),
+						li->parent.lock());
+					auto set = std::make_shared<SetInstruction>(var, li->imm);
+					auto l = std::make_shared<LoadRInstruction>(var, li->rd, li->size);
+					function.insertBefore(li, set)->setDebug(li->debugIndex)->extract();
+					function.insertBefore(li, l)->setDebug(li->debugIndex)->extract();
+					to_remove.push_back(li);
+				} else if (li->size == 3) {
+					// Sometimes, if you have a packed struct, you might get something like
+					//     %14 = load i24, i24* %13
+					// which normally translates to something like
+					//     [$t9] -> $ta /3
+					// Why has no instructions for 24-bit loads, so we have to apply a mask here.
+					// For stores, which I'm too lazy to do now because I mercifully see no 24-bit stores in Thurisaz,
+					// we'd have to break the store into multiple byte-sized stores, and that would be kind of a pain.
+					li->size = 4;
+					auto andi = std::make_shared<AndIInstruction>(li->rd, 0xffffff, li->rd);
+					// I don't use insertAfter all that often. Neat.
+					function.insertAfter(li, andi)->setDebug(li->debugIndex)->extract();
+				} else continue;
+			} else if (auto l = std::dynamic_pointer_cast<LoadRInstruction>(instruction)) {
+				if (l->size == 3) {
+					l->size = 4;
+					auto andi = std::make_shared<AndIInstruction>(l->rd, 0xffffff, l->rd);
+					function.insertAfter(l, andi)->setDebug(l->debugIndex)->extract();
+				}
 			} else if (auto si = std::dynamic_pointer_cast<StoreIInstruction>(instruction)) {
 				if (si->size != 4)
 					continue;
