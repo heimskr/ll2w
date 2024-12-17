4c62f1009d84511463aa058b3d3b2dbcc9b0a7cd
Kai Tamkun <ktamkun@ucsc.edu>
2024-12-16 23:52:47 -0800

    Format FunctionHeader.cpp

--- src/parser/FunctionHeader.cpp
+++ src/parser/FunctionHeader.cpp
@@ -25,38 +25,40 @@ namespace LL2W {
 
 		if (_linkage) {
 			const std::string &link = *_linkage->lexerInfo;
-			for (const std::pair<const Linkage, std::string> &pair: linkage_map)
+			for (const std::pair<const Linkage, std::string> &pair: linkage_map) {
 				if (link == pair.second) {
 					linkage = pair.first;
 					break;
 				}
+			}
 		}
 
 		if (_preemption) {
-			if (*_preemption->lexerInfo == "dso_preemptable")
+			if (*_preemption->lexerInfo == "dso_preemptable") {
 				preemption = Preemption::DsoPreemptable;
-			else if (*_preemption->lexerInfo == "dso_local")
+			} else if (*_preemption->lexerInfo == "dso_local") {
 				preemption = Preemption::DsoLocal;
-			else throw std::runtime_error("Invalid preemption: " + *_preemption->lexerInfo);
+			} else {
+				throw std::runtime_error("Invalid preemption: " + *_preemption->lexerInfo);
+			}
 		}
 
 		if (_visibility) {
-			visibility = *_visibility->lexerInfo == "hidden"? Visibility::Hidden :
-				(*_visibility->lexerInfo == "protected"? Visibility::Protected : Visibility::Default);
+			visibility = *_visibility->lexerInfo == "hidden"? Visibility::Hidden : (*_visibility->lexerInfo == "protected"? Visibility::Protected : Visibility::Default);
 		}
 
 		if (_dll_storage_class) {
-			dllStorageClass = *_dll_storage_class->lexerInfo == "dllimport"?
-				DllStorageClass::Import : DllStorageClass::Export;
+			dllStorageClass = *_dll_storage_class->lexerInfo == "dllimport"? DllStorageClass::Import : DllStorageClass::Export;
 		}
 
 		if (_cconv) {
 			const std::string &cc = *_cconv->lexerInfo;
-			for (const std::pair<const CConv, std::string> &pair: cconv_map)
+			for (const std::pair<const CConv, std::string> &pair: cconv_map) {
 				if (cc == pair.second) {
 					cconv = pair.first;
 					break;
 				}
+			}
 		}
 
 		if (_retattrs) {
@@ -77,8 +79,9 @@ namespace LL2W {
 					int bytes = retattr->at(0)->atoi();
 					if (deref == Deref::DereferenceableOrNull && new_deref == Deref::Dereferenceable) {
 						// If dereferenceable_or_null(x) -> dereferenceable(y), set bytes to max(x, y).
-						if (dereferenceableBytes < bytes)
+						if (dereferenceableBytes < bytes) {
 							dereferenceableBytes = bytes;
+						}
 						deref = new_deref;
 					} else if (deref == Deref::Default) {
 						deref = new_deref;
@@ -88,100 +91,140 @@ namespace LL2W {
 			}
 		}
 
-		if (debug)
+		if (debug) {
 			debugIndex = debug->front()->atoi();
+		}
 
 		returnType = getType(type);
 
 		if (unnamed_addr) {
 			const std::string &uatype = *unnamed_addr->lexerInfo;
-			if (uatype == "local_unnamed_addr")
+			if (uatype == "local_unnamed_addr") {
 				unnamedAddr = UnnamedAddr::LocalUnnamed;
-			else if (uatype == "unnamed_addr")
+			} else if (uatype == "unnamed_addr") {
 				unnamedAddr = UnnamedAddr::Unnamed;
-			else throw std::runtime_error("Invalid lexerinfo for unnamed_addr: " + uatype);
+			} else {
+				throw std::runtime_error("Invalid lexerinfo for unnamed_addr: " + uatype);
+			}
 		}
 
-		if (_fnattrs->symbol == LLVMTOK_DECIMAL)
+		if (_fnattrs->symbol == LLVMTOK_DECIMAL) {
 			fnattrsIndex = _fnattrs->atoi();
-		else if (_fnattrs->symbol == LLVM_FNATTR_LIST)
+		} else if (_fnattrs->symbol == LLVM_FNATTR_LIST) {
 			for (ASTNode *fnattr: _fnattrs->children) {
 				const std::string &fnattr_name = *fnattr->lexerInfo;
-				for (const std::pair<const FnAttr, std::string> &pair: fnattr_map)
+				for (const std::pair<const FnAttr, std::string> &pair: fnattr_map) {
 					if (fnattr_name == pair.second) {
 						fnattrs.insert(pair.first);
 						break;
 					}
+				}
 			}
-		else
+		} else {
 			throw std::runtime_error("Bad symbol for fnattrs node: " + std::string(parser->getName(_fnattrs->symbol)));
-
-		if (_section)
-			section = _section->at(0)->lexerInfo;
-
-		if (_comdat) {
-			if (_comdat->empty())
-				comdat = name;
-			else
-				comdat = _comdat->at(0)->lexerInfo;
 		}
 
-		if (_align)
-			align = _align->at(0)->atoi();
+		if (_section) {
+			section = _section->at(0)->lexerInfo;
+		}
 
-		if (_personality)
+		if (_comdat) {
+			if (_comdat->empty()) {
+				comdat = name;
+			} else {
+				comdat = _comdat->at(0)->lexerInfo;
+			}
+		}
+
+		if (_align) {
+			align = _align->at(0)->atoi();
+		}
+
+		if (_personality) {
 			personality = std::make_shared<Constant>(_personality->at(0));
+		}
 	}
 
 	std::string FunctionHeader::debugExtra() const {
 		std::stringstream out;
+
 		out << " \e[0;33m" << std::string(*returnType);
 		out << " \e[0;32m" << *name << "\e[36m";
 		out << "\e[0;94m(";
+
 		for (auto iter = arguments->arguments.begin(), end = arguments->arguments.end(); iter != end; ++iter) {
-			if (iter != arguments->arguments.begin())
+			if (iter != arguments->arguments.begin()) {
 				out << "\e[2m,\e[22m ";
+			}
+
 			const FunctionArgument &arg = *iter;
 			out << std::string(*arg.type);
-			for (ParAttr parattr: arg.parattrs)
+
+			for (ParAttr parattr: arg.parattrs) {
 				out << " " << parattr_map.at(parattr);
-			if (arg.originalName)
+			}
+
+			if (arg.originalName) {
 				out << " " << *arg.originalName;
+			}
 		}
-		if (arguments->ellipsis)
+
+		if (arguments->ellipsis) {
 			out << (arguments->arguments.empty()? "..." : ", ...");
+		}
+
 		out << "\e[94m)\e[0;36m";
-		if (linkage != Linkage::Default)
+
+		if (linkage != Linkage::Default) {
 			out << " " << linkage_map.at(linkage);
+		}
+
 		switch (visibility) {
 			case Visibility::Hidden:    out << " hidden"; break;
 			case Visibility::Protected: out << " protected"; break;
-			default:;
+			default: break;
 		}
+
 		switch (dllStorageClass) {
 			case DllStorageClass::Import: out << " import"; break;
 			case DllStorageClass::Export: out << " export"; break;
-			default:;
+			default: break;
 		}
-		if (cconv != CConv::ccc)
+
+		if (cconv != CConv::ccc) {
 			out << " " << cconv_map.at(cconv);
-		for (RetAttr retattr: retattrs)
+		}
+
+		for (RetAttr retattr: retattrs) {
 			out << " " << retattr_map.at(retattr);
-		if (deref == Deref::Dereferenceable)
+		}
+
+		if (deref == Deref::Dereferenceable) {
 			out << " dereferenceable(" << dereferenceableBytes << ")";
-		else if (deref == Deref::DereferenceableOrNull)
+		} else if (deref == Deref::DereferenceableOrNull) {
 			out << " dereferenceable_or_null(" << dereferenceableBytes << ")";
-		if (unnamedAddr == UnnamedAddr::Unnamed)
+		}
+
+		if (unnamedAddr == UnnamedAddr::Unnamed) {
 			out << " unnamed_addr";
-		else if (unnamedAddr == UnnamedAddr::LocalUnnamed)
+		} else if (unnamedAddr == UnnamedAddr::LocalUnnamed) {
 			out << " local_unnamed_addr";
-		for (FnAttr fnattr: fnattrs)
+		}
+
+		for (FnAttr fnattr: fnattrs) {
 			out << " " << fnattr_map.at(fnattr);
-		if (fnattrsIndex != -1)
+		}
+
+		if (fnattrsIndex != -1) {
 			out << " #" << fnattrsIndex;
-		if (personality)
+		}
+
+		if (personality) {
 			out << " personality " << std::string(*personality);
+		}
+
 		out << "\e[0m";
+
 		return out.str();
 	}
 }
