330a8acaa0fdd0cdc78f08d44f80ea138643d944
Kai Tamkun <ktamkun@ucsc.edu>
2024-12-25 02:39:08 -0800

    Ignore llvm.assume

--- src/pass/IgnoreIntrinsics.cpp
+++ src/pass/IgnoreIntrinsics.cpp
@@ -7,9 +7,9 @@
 namespace LL2W::Passes {
 	int ignoreIntrinsics(Function &function) {
 		Timer timer("IgnoreInstrinsics");
-		std::list<InstructionPtr> to_remove;
+		std::vector<InstructionPtr> to_remove;
 
-		for (InstructionPtr &instruction: function.linearInstructions) {
+		for (const InstructionPtr &instruction: function.linearInstructions) {
 			LLVMInstruction *llvm = dynamic_cast<LLVMInstruction *>(instruction.get());
 			if (!llvm || llvm->getNodeType() != NodeType::Call) {
 				continue;
@@ -22,16 +22,21 @@ namespace LL2W::Passes {
 
 			GlobalValue *global_name = dynamic_cast<GlobalValue *>(call->name.get());
 
+			const static std::unordered_set<std::string> ignore{
+				"@llvm.experimental.noalias.scope.decl",
+				"@llvm.assume",
+			};
+
 			std::string_view name(*global_name->name);
 
 			if (name.starts_with("@llvm.lifetime.") || name.starts_with("@llvm.dbg.")) {
 				to_remove.push_back(instruction);
-			} else if (name == "@llvm.experimental.noalias.scope.decl") {
+			} else if (ignore.contains(*global_name->name)) {
 				to_remove.push_back(instruction);
 			}
 		}
 
-		for (InstructionPtr &instruction: to_remove) {
+		for (const InstructionPtr &instruction: to_remove) {
 			function.remove(instruction);
 		}
 
